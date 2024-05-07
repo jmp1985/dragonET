fa443e014d872ce0c10206e6884e4c64e9ba129e
James Parkhurst <james.parkhurst@diamond.ac.uk>
2024-05-07 10:23:23 +0100

    Add new command line programs

--- src/dragonET/command_line/__init__.py
+++ src/dragonET/command_line/__init__.py
@@ -1,3 +1,4 @@
+from dragonET.command_line._new import *  # noqa
 from dragonET.command_line._pick import *  # noqa
 from dragonET.command_line._reconstruct import *  # noqa
 from dragonET.command_line._refine import *  # noqa
--- src/dragonET/command_line/_pick.py
+++ src/dragonET/command_line/_pick.py
@@ -5,12 +5,14 @@
 #
 # Author: James Parkhurst
 #
+import itertools
 import time
 from argparse import ArgumentParser
 from typing import List
 
 import mrcfile
 import napari
+import numpy as np
 import yaml
 
 __all__ = ["pick"]
@@ -49,11 +51,23 @@ def get_parser(parser: ArgumentParser = None) -> ArgumentParser:
         ),
     )
     parser.add_argument(
-        "-c",
-        "--contours",
+        "-o",
+        "--contours_out",
         type=str,
         default="contours.yaml",
-        dest="contours",
+        dest="contours_out",
+        help=(
+            """
+            A YAML file describing the picked point coordinates.
+            """
+        ),
+    )
+    parser.add_argument(
+        "-i",
+        "--contours_in",
+        type=str,
+        default=None,
+        dest="contours_in",
         help=(
             """
             A YAML file describing the picked point coordinates.
@@ -76,7 +90,8 @@ def pick_impl(args):
     # Do the work
     _pick(
         args.projections,
-        args.contours,
+        args.contours_out,
+        args.contours_in,
     )
 
     # Write some timing stats
@@ -91,11 +106,27 @@ def pick(args: List[str] = None):
     pick_impl(get_parser().parse_args(args=args))
 
 
-def _pick(projections_filename: str, contours_filename: str):
+def _pick(
+    projections_filename: str,
+    contours_out_filename: str,
+    contours_in_filename: str = None,
+):
     """
     Pick the fiduccials manually
 
     """
+
+    def read_projections(filename):
+        print("Reading projections from %s" % filename)
+        return mrcfile.mmap(filename)
+
+    def read_contours(filename):
+        print("Reading contours from %s" % filename)
+        if filename:
+            contours = yaml.safe_load(open(filename))["contours"]
+        else:
+            contours = None
+        return contours
 
     def write_contours(filename, contours):
         print("Writing contours to %s" % filename)
@@ -105,27 +136,44 @@ def _pick(projections_filename: str, contours_filename: str):
     viewer = napari.Viewer()
 
     # Load the projections data
-    projections_file = mrcfile.mmap(projections_filename)
+    projections_file = read_projections(projections_filename)
 
     # Add the image layer
     viewer.add_image(projections_file.data, name="Projections")
+
+    # Read the contours
+    contours = read_contours(contours_in_filename)
+
+    # Add the contours to the viewer
+    contours = sorted(contours, key=lambda x: x[0])
+    for index, group in itertools.groupby(contours, lambda x: x[0]):
+        points = [x[1:] for x in group]
+        name = "Points [%d]" % index if index > 0 else "Points"
+        viewer.add_points(points, name=name)
 
     # Start Napari
     napari.run()
 
     # Get the points layers
     contours = []
-    for index, layer in enumerate(viewer.layers):
+    index = 0
+    for layer in viewer.layers:
         if isinstance(layer, napari.layers.Points):
-            points = layer.data
-            z = points[:, 0]
-            print("Contour %d has %d points" % (index, points.shape[0]))
-            if points.shape[0] == 1:
-                print("- Warning: contour %d has only 1 point" % index)
-            if len(z) != len(set(z)):
-                print("- Warning: Contour %d has more than 1 point per image" % index)
-            for p in points.tolist():
-                contours.append((index, p[0], p[1], p[2]))
+            points2 = layer.data
+            if points2.shape[0] == 0:
+                print("- Warning: skipping contour with no points")
+            else:
+                print("Contour %d has %d points" % (index, points2.shape[0]))
+                points2 = np.array(sorted(points2.tolist(), key=lambda x: x[0]))
+                if points2.shape[0] == 1:
+                    print("- Warning: contour %d has only 1 point" % index)
+                if len(points2[:, 0]) != len(set(points2[:, 0])):
+                    print(
+                        "- Warning: Contour %d has more than 1 point per image" % index
+                    )
+                for p in points2.tolist():
+                    contours.append((index, p[0], p[1], p[2]))
+                index += 1
 
     # Write the contours
-    write_contours(contours_filename, {"contours": contours})
+    write_contours(contours_out_filename, {"contours": contours})
