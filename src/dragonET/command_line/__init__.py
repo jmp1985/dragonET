75e1874654c9c411e00950778673433cef108bb1
James Parkhurst <james.parkhurst@diamond.ac.uk>
2024-05-14 15:26:43 +0100

    Added a rot90 command

--- src/dragonET/command_line/__init__.py
+++ src/dragonET/command_line/__init__.py
@@ -2,4 +2,5 @@ from dragonET.command_line._new import *  # noqa
 from dragonET.command_line._pick import *  # noqa
 from dragonET.command_line._reconstruct import *  # noqa
 from dragonET.command_line._refine import *  # noqa
+from dragonET.command_line._rot90 import *  # noqa
 from dragonET.command_line._select_sample_axis import *  # noqa
